d72b9f39a12d60e76a821d504027e9f8c6bb97cb
Abraham Isaac Durán <aid.mest@gmail.com>
2019-09-04 04:56:31 -0400

    Add NetworkParameters

---
+++ Movies/Supporting Files/NetworkParameters.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkParameters.swift
+//  Movies
+//
+//  Created by Abraham Isaac Durán on 9/4/19.
+//  Copyright © 2019 Abraham Isaac Durán. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkParameters {
+    let apiUrl: URL
+    let hostName: String
+    let timeoutForRequest: Double
+    let headers: [String: String]
+    
+    static func appConfiguration() throws -> NetworkParameters {
+        let info = Bundle.main.object(forInfoDictionaryKey: "NetworkParameters") as? [String: Any]
+        guard let host = info?["HostName"] as? String,
+            let urlString = info?["URL"] as? String,
+            let timeout = info?["TimeoutForRequest"] as? Double,
+            let headers = info?["AuthKeys"] as? [String: String]
+            else { fatalError("Missing Network Parameters.") }
+        
+        guard let apiUrl = URL(string: urlString)
+        else { fatalError("Invalid API URL.") }
+        
+        return self.init(apiUrl: apiUrl, hostName: host, timeoutForRequest: timeout, headers: headers)
+    }
+}
