4e68abfa8165567635181a1a318e4a92739adecd
Abraham Isaac Durán <aid.mest@gmail.com>
2019-09-04 20:55:09 -0400

    Add synchronous version of the MoviesDataSource functions

--- Movies/Data/Sources/MoviesDataSource.swift
+++ Movies/Data/Sources/MoviesDataSource.swift
@@ -16,4 +16,67 @@ protocol MoviesDataSource {
     func searchMovies(with query: String, completion: @escaping (ReadMoviesOperation) -> Void)
     func getFavoriteMovies(completion: @escaping (ReadMoviesOperation) -> Void)
     func getWatchList(completion: @escaping (ReadMoviesOperation) -> Void)
+    
+    func discoverMovies(by year: Year) -> ReadMoviesOperation
+    func getDetail(for movie: Movie) -> Result<MovieDetail, Error>
+    func searchMovies(with query: String) -> ReadMoviesOperation
+    func getFavoriteMovies() -> ReadMoviesOperation
+    func getWatchList() -> ReadMoviesOperation
+}
+
+extension MoviesDataSource {
+    func discoverMovies(by year: Year) -> ReadMoviesOperation {
+        var readResult: ReadMoviesOperation!
+        let group = DispatchGroup()
+        group.enter()
+        discoverMovies(by: year) { result in
+            readResult = result
+        }
+        group.wait()
+        return readResult
+    }
+    
+    func getDetail(for movie: Movie) -> Result<MovieDetail, Error> {
+        var readResult: Result<MovieDetail, Error>!
+        let group = DispatchGroup()
+        group.enter()
+        getDetail(for: movie) { result in
+            readResult = result
+        }
+        group.wait()
+        return readResult
+    }
+    
+    func searchMovies(with query: String) -> ReadMoviesOperation {var readResult: ReadMoviesOperation!
+        let group = DispatchGroup()
+        group.enter()
+        searchMovies(with: query) { result in
+            readResult = result
+        }
+        group.wait()
+        return readResult
+        
+    }
+    
+    func getFavoriteMovies() -> ReadMoviesOperation {
+        var readResult: ReadMoviesOperation!
+        let group = DispatchGroup()
+        group.enter()
+        getFavoriteMovies { result in
+            readResult = result
+        }
+        group.wait()
+        return readResult
+    }
+    
+    func getWatchList() -> ReadMoviesOperation {
+        var readResult: ReadMoviesOperation!
+        let group = DispatchGroup()
+        group.enter()
+        getWatchList { result in
+            readResult = result
+        }
+        group.wait()
+        return readResult
+    }
 }
