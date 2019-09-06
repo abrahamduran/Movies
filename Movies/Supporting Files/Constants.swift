6e402b32dc43ae4104cc7466070e779a3a8792fb
Abraham Isaac Durán <aid.mest@gmail.com>
2019-09-06 10:00:34 -0400

    Define constants

---
+++ Movies/Supporting Files/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Movies
+//
+//  Created by Abraham Isaac Durán on 9/5/19.
+//  Copyright © 2019 Abraham Isaac Durán. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let LOWRES_URL = "https://image.tmdb.org/t/p/w185"
+let HIGHRES_URL = "https://image.tmdb.org/t/p/w780"
+let POSTER_ASPECT_RATIO: CGFloat = 1.5
+let GRID_COLUMNS: CGFloat = 2
